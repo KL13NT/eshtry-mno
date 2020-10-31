45e914ff04f67c9ea8440fa1916770ec7dcf6f09
Nabil <nabil.tharwat@outlook.com>
2020-10-31 19:46:51 +0200

    config: setup all contributors, vercel, and tailwind done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
-const { fontFamily: defaultFonts } = require('tailwindcss/defaultTheme')
-
 module.exports = {
 	future: {
 		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: false
 	},
 
-	purge: ['./src/**/*.jsx', './src/**/*.css', './src/**/*.js'],
+	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
 
 	theme: {
-		colors: {
-			blue: {
-				300: '#F5F7FF',
-				600: '#0083FF'
+		extend: {
+			colors: {
+				blue: {
+					300: '#F5F7FF',
+					400: '#e5f2ff',
+					600: '#0083FF'
+				},
+				transparent: 'transparent'
 			},
-			transparent: 'transparent'
-		},
-		fontFamily: {
-			rtl:
-				'Tajawal, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji'
+			fontFamily: {
+				rtl:
+					'Tajawal, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji'
+			}
 		}
-	}
+	},
+	variants: {},
+	plugins: []
 }
