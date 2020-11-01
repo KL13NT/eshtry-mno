b41bfc0aa13b8c98ff84c6299d8709908328de26
Nabil <nabil.tharwat@outlook.com>
2020-11-01 12:05:05 +0200

    feat: improved a11y for navigation bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,17 +6,19 @@ import Footer from './Footer.jsx'
 
 export default function Layout({ children }) {
 	return (
-		<div className='font-rtl rtl px-4 pb-24 mt-4'>
+		<div className='font-rtl rtl px-4 pb-24'>
 			<noscript>
 				<Toast>لازم تشغل جافاسكربت عندك عشان الموقع يشتغل كويس.</Toast>
 			</noscript>
 
-			<nav className='flex justify-evenly md:justify-around mx-auto md:w-1/5'>
+			<nav className='flex justify-evenly md:justify-around mx-auto md:w-2/5 bg-blue-600 p-2 rounded-lg text-black text-xl text-center bg-opacity-25 mt-2'>
 				<Route href='/'>
-				<img src='/uranus.svg' alt='home icon' className='w-12' />
+					<img src='/uranus.svg' alt='home icon' className='w-12 inline' />
+					<span className='block mt-2'>الرئيسية</span>
 				</Route>
 				<Route href='/search'>
-					<img src='/search.svg' alt='search icon' className='w-12' />
+					<img src='/search.svg' alt='search icon' className='w-12 inline' />
+					<span className='block mt-2'>البحث</span>
 				</Route>
 			</nav>
 
--- components/Route.jsx
+++ components/Route.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 
 export default function Route({ href, children }) {
-	const base = 'p-4 rounded block focus:bg-blue-600'
+	const base =
+		'p-4 rounded block focus:bg-blue-600 focus:text-white transition-colors duration-200 focus:no-underline outline-none'
 
 	return (
 		<Link href={href}>
